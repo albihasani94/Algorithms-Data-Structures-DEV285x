d5e2ff93889c8562e1478b46be073657c05330cc
ahasani <albin.hasani@ikubinfo.al>
2017-08-27 19:13:05 +0200

    Linear search implementation

---
+++ src/LinearSearch.java
@@ -0,0 +1,24 @@
+/**
+ * @author ahasani
+ */
+class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int[] lst = {4, 9, 7, 1, 3, 6, 5};
+
+        int index = linearSearch(lst, 5);
+
+        System.out.println(index);
+    }
+
+    static int linearSearch(int[] lst, int number) {
+        int index=0;
+        for(int i=0; i<lst.length; i++) {
+            if (lst[i] == number) {
+                index=i;
+            }
+        }
+        return index;
+    }
+}
